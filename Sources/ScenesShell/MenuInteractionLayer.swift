303df74a447c5872f35b619fade5dc7c900635f3
Kush Gupta <kush.gupta@student.allenisd.org>
2021-05-16 19:00:22 -0500

    Updated MenuInteractionLayer and ShellDirector

--- Sources/ScenesShell/MenuInteractionLayer.swift
+++ Sources/ScenesShell/MenuInteractionLayer.swift
@@ -1,1 +1,33 @@
 import Scenes
+import Igis
+import ScenesControls
+
+
+class MenuInteractionLayer : Layer {
+    var didRender = false
+
+
+
+    init() {
+        super.init(name:"Menu Interaction")
+
+        let startGridshotButton = Button(name:"startGridshot", labelString: "Play Gridshot",
+                                         topLeft: Point(x: 50, y: 50))
+        startGridshotButton.clickHandler = onStartGridShotButtonClickHandler
+        insert(entity: startGridshotButton, at: .front)
+        
+    }
+    /*
+    override func render(canvas:Canvas) {
+        if let canvasSize = canvas.canvasSize, !didRender{
+            
+            didRender = true
+        } 
+    }
+    */
+
+
+    private func onStartGridshotButtonClickHandler(control: Control, localLocation: Point) {
+        ShellDirector().startGridshot()
+    }
+}
--- Sources/ScenesShell/ShellDirector.swift
+++ Sources/ScenesShell/ShellDirector.swift
@@ -4,13 +4,35 @@ import Scenes
 /*
  This class is primarily responsible for transitioning between Scenes.
  At a minimum, it must enqueue the first Scene.
-*/
+ */
+
+// GridshotScene, MenuScene, MainScene
+
+/*
+ Information about the Director and how Scenes work:
+ Scenes are constructed of different Layers.
+ Layers are constructed of different RenderableEntitys, which are objects responsible for providing the rendering and interaction for a Layer.
+ 
+ Director's first action is to enqueue a scene.
+ The scene executes until transition method is invoked to 'transition' to the next scene. 
+ */
+
 class ShellDirector : Director {
     required init() {
         super.init()
-        enqueueScene(scene:MainScene())
+        enqueueScene(scene:MenuScene())
     }
 
+
+    // The two lines below enqueue the Main Menu scene and then transitions to it instantly.
+    func startGridshot() {
+        enqueueScene(scene:GridshotScene())
+        transitionToNextScene()
+    }
+    
+    // if MenuScene.swap() is true, then transition scene here. In menu, add button that makes swap function true when it is pressed
+    // Below code is for transition to different game scenes.
+    
     override func framesPerSecond() -> Int {
         return 30
     }
