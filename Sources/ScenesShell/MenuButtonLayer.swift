146bf51c942ba36798eb63ae1bfc41f4d9cb580b
Akshat Sharma <akshat.sharma@student.allenisd.org>
2021-05-16 19:02:05 -0500

    Adding a class for the git menu interaction layer.

--- Sources/ScenesShell/MenuButtonLayer.swift
+++ Sources/ScenesShell/MenuButtonLayer.swift
@@ -1,1 +1,22 @@
 import Scenes
+import Igis
+
+class menuButtonLayer : Layer {
+//        var didRender = false
+
+
+
+    init() {
+        super.init(name:"Menu Interaction")
+        
+    }
+    /*
+    override func render(canvas:Canvas) {
+        if let canvasSize = canvas.canvasSize, !didRender{
+            
+            didRender = true
+        } 
+    }
+    */
+}
+
