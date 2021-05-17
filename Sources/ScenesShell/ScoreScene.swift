d7ef48771a4c2af42eb693753df58416e0a7f33b
Kush Gupta <kush.gupta@student.allenisd.org>
2021-05-16 21:15:52 -0500

    Updated ScoreScene file

--- Sources/ScenesShell/ScoreScene.swift
+++ Sources/ScenesShell/ScoreScene.swift
@@ -6,7 +6,7 @@ import Scenes
    A Scene is comprised of one or more Layers.
    Layers are generally added in the constructor.
  */
-class MenuScene : Scene {
+class ScoreScene : Scene {
 
     /* Scenes typically include one or more Layers.
        A common approach is to use three Layers:
@@ -18,7 +18,7 @@ class MenuScene : Scene {
     let scoreInteractionLayer = ScoreInteractionLayer()
     init() {
         // Using a meaningful name can be helpful for debugging
-        super.init(name:"Menu Scene")
+        super.init(name:"Score Scene")
 
         // We insert our Layers in the constructor
         // We place each layer in front of the previous layer
