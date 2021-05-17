0112438cf951f8a00b7376d67d52d9ac31df96f2
Kush Gupta <kush.gupta@student.allenisd.org>
2021-05-17 03:45:34 -0500

    Attempted add different timer buttons for gamemodes

--- Sources/ScenesShell/GridshotInteractionLayer.swift
+++ Sources/ScenesShell/GridshotInteractionLayer.swift
@@ -7,17 +7,20 @@ class GridshotInteractionLayer : Layer {
 
     var target = GridshotTarget(center:Point(x:960, y:500), radiusX: 20, radiusY: 20) // 960, 500
 
-    public var timeSet : Int?
-    
-    let timer = Timer(enqueueScene:ScoreScene(), timeLimit:20)
+
+    let timer = SettableTimer()
+
+    //let timer = Timer(enqueueScene:ScoreScene(), timeLimit:15)
 
     //var didRender = false
     
     public init() {
         super.init(name:"Gridshot Interaction")
+        SettableTimer().setTime(endTime:10)
 
         insert(entity: target, at:.front)
         insert(entity: timer, at:.front)
+        print(SettableTimer().endTimeLimit)
     }
 
 }
