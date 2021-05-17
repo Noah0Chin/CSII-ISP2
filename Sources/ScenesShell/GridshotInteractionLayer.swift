01e9af0646e3debfd2687a3fa066629a33532f55
Elvin Li <elvin.li@student.allenisd.org>
2021-05-16 22:59:34 -0500

    Adding the settings for Gridshot gamemode. Created random coordinates and began spawn system for targets.

--- Sources/ScenesShell/GridshotInteractionLayer.swift
+++ Sources/ScenesShell/GridshotInteractionLayer.swift
@@ -1,40 +1,34 @@
 import Scenes
 import Igis
+import Foundation
 import ScenesControls
 
 class GridshotInteractionLayer : Layer {
-   // var didRender = false
-//    var xPosition : Int
-  //  var yPosition : Int
 
-    init() {
+    var target = Target(center:Point(x:960, y:500), radiusX: 20, radiusY: 20)
+    var didRender = false
+    
+    public init() {
         super.init(name:"Gridshot Interaction")
+        insert(entity: target, at:.front)       
+    }
 
+    // Sets random coordinates for a target
+    public func delineateTarget() {
+
+        let xPosition = Int.random(in: 200 ..< 1720)
+        let yPosition = Int.random(in: 200 ..< 880)
+
+        target = Target(center:Point(x:xPosition, y:yPosition), radiusX: 20, radiusY: 20)
+    }
+
+    // Verifies and inserts a target when the previous target is destroyed
+    public func spawnTarget() {
         
-        let getNewPositionButton = Button(name: "NewPosition", labelString: "New Position", topLeft: Point(x: 500, y:500))
-        getNewPositionButton.clickHandler = getNewPositionButtonClickHandler
-        insert(entity: getNewPositionButton, at: .front)
+        delineateTarget()
+        insert(entity: target, at:.front)        
     }
 
-    // Render the objects and potentially the canvas where the objects will spawn
-/*    override func render(canvas:Canvas) {
-        if let canvasSize = canvas.canvasSize, !didRender {
-            
-            // render one target
-            
-            didRender = true
-        }
-    }
-*/
-    func getNewPosition() {
-    }
-
-    func getNewPositionButtonClickHandler(control: Control, localLocation: Point) {
-        getNewPosition()
-    }
+    
     
 }
-
-
-
-    
