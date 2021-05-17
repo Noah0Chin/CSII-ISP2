6e174ca6aeeb3001896772d475a94b8d1d66101e
Kush Gupta <kush.gupta@student.allenisd.org>
2021-05-17 01:30:16 -0500

    Added Trackshot gamemode and button in the Main Menu

--- Sources/ScenesShell/MenuInteractionLayer.swift
+++ Sources/ScenesShell/MenuInteractionLayer.swift
@@ -6,22 +6,46 @@ import ScenesControls
 class MenuInteractionLayer : Layer {
     var didRender = false
 
+    let gamesArray = [GridshotScene(), WipeshotScene()]
+    
     init() {
         super.init(name:"Menu Interaction")
 
+        let startRandomButton = Button(name:"startRandom", labelString: "Random",
+                                         topLeft: Point(x: 50, y: 25))
+        startRandomButton.clickHandler = onStartRandomButtonClickHandler
+        insert(entity: startRandomButton, at: .front)        
+        
         let startGridshotButton = Button(name:"startGridshot", labelString: "Play Gridshot",
-                                         topLeft: Point(x: 50, y: 50))
+                                         topLeft: Point(x: 50, y: 100))
         startGridshotButton.clickHandler = onStartGridshotButtonClickHandler
         insert(entity: startGridshotButton, at: .front)
 
         let startWipeshotButton = Button(name:"startWipeshot", labelString: "Play Wipeshot",
-                                         topLeft: Point(x: 50, y: 100))
+                                         topLeft: Point(x: 50, y: 150))
         startWipeshotButton.clickHandler = onStartWipeshotButtonClickHandler
-        insert(entity: startWipeshotButton, at: .front)        
+        insert(entity: startWipeshotButton, at: .front)
+
+        let startTrackshotButton = Button(name:"startTrackshot", labelString: "Play Trackshot",
+                                         topLeft: Point(x: 50, y: 200))
+        startTrackshotButton.clickHandler = onStartTrackshotButtonClickHandler
+        insert(entity: startTrackshotButton, at: .front)
+
     }
 
+    func createButton(name:String) -> String {
+        let test = "private func onStart\(name)ButtonClickHandler(control: Control, localLocation: Point) {"
+        return test
+    }
+    
+    //createButton(name:"TestGame")
+
+    private func onStartRandomButtonClickHandler(control: Control, localLocation: Point) {
+        director.enqueueScene(scene:gamesArray.randomElement()!)
+        director.transitionToNextScene()
+    }
+    
     private func onStartGridshotButtonClickHandler(control: Control, localLocation: Point) {    
-        //ShellDirector().startGridshot()
         director.enqueueScene(scene:GridshotScene())
         director.transitionToNextScene()
     }
@@ -30,4 +54,11 @@ class MenuInteractionLayer : Layer {
         director.enqueueScene(scene:WipeshotScene())
         director.transitionToNextScene()
     }
+
+    private func onStartTrackshotButtonClickHandler(control: Control, localLocation: Point) {
+        director.enqueueScene(scene:gamesArray.randomElement()!)
+        director.transitionToNextScene()
+    }
+
+    
 }
