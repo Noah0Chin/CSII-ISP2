36c8112e9d75ee55eb0e9ddfd8c30300e641a44a
Kush Gupta <kush.gupta@student.allenisd.org>
2021-05-17 13:08:59 -0500

    Fixed bug in GridshotInteractionLayer

--- Sources/ScenesShell/GridshotInteractionLayer.swift
+++ Sources/ScenesShell/GridshotInteractionLayer.swift
@@ -7,16 +7,16 @@ class GridshotInteractionLayer : Layer {
 
     var target = GridshotTarget(center:Point(x:960, y:500), radiusX: 20, radiusY: 20) // 960, 500
 
-<<<<<<< HEAD
+
     public var timeSet : Int?
     
-    let timer = Timer(enqueueScene:ScoreScene(), timeLimit:45)
-=======
+    //let timer = Timer(enqueueScene:ScoreScene(), timeLimit:45)
+
 
     let timer = SettableTimer()
 
     //let timer = Timer(enqueueScene:ScoreScene(), timeLimit:15)
->>>>>>> 0112438cf951f8a00b7376d67d52d9ac31df96f2
+
 
     //var didRender = false
     
